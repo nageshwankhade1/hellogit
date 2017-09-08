9dea0ed57779b075d5133c24099695bef4f601a3
nagesh <nagesh@DESKTOP-I0RA1GH>
2017-09-09 01:51:14 +0530

    committed...
    Change-Id: I9cc344e5f5e2f53863150e6ebd1cb076d938c64f
    Signed-off-by: nagesh <nagesh@DESKTOP-I0RA1GH>

---
+++ Test/src/com/nmw/main/TestMain.java
@@ -0,0 +1,12 @@
+package com.nmw.main;
+
+public class TestMain {
+
+	public static void main(String[] args) 
+	{
+		//greeting...
+		System.out.println("Hello Nagesh...");
+		System.out.println("Nagesh...");
+	}
+
+}
